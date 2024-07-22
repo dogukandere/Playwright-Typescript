baf14f66ac45767834a9cc09d7cb4c1702e07dff
Doğukan Dere <102256801+dogukandere@users.noreply.github.com>
2024-07-22 17:20:46 +0300

    change

--- tests/home-test.spec.ts
+++ tests/home-test.spec.ts
@@ -1,9 +1,10 @@
 import { test, expect } from '@playwright/test';
 import { Pages } from "../pages/pages";
 
-test('Search product and verify buttons', async({ page })=>{
+test('Search product and verify buttons', async({page})=>{
   const pages = Pages(page);
   await pages.homePage.goToPage();
+  await expect(page).toHaveURL("https://www.ebay.com/");
   await expect(page).toHaveTitle("Electronics, Cars, Fashion, Collectibles & More | eBay");
   await pages.homePage.verifyButtonsOnHomePage();
   await pages.homePage.searchProduct("ipad");
